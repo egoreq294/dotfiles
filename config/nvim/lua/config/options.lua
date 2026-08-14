7aa05a49b5f923621bd14c3059a4dc02d5413237
egoreq <egoreq@bk.ru>
2026-08-14 22:12:40 +0400

    fix

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,11 +1,4 @@
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
+if vim.env.SSH_TTY then
+  vim.g.clipboard = "osc52"
+  vim.opt.clipboard = "unnamedplus"
+end
