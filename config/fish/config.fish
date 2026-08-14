2726679bff244d905aace492433fcefd08195367
egoreq <egoreq@bk.ru>
2026-08-14 22:18:17 +0400

    fix

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,9 +5,6 @@ if status is-interactive
     set -x GOPATH $HOME/.cache/go
     set -x GOCACHE $HOME/.cache/go-build
 
-    # Выключаем стандартное приветствие
-    # set -g fish_greeting "fastfetch"
-
     function fish_greeting
         if command -q fastfetch
             fastfetch
@@ -26,14 +23,12 @@ if status is-interactive
     alias restart-audio='systemctl --user restart wireplumber pipewire pipewire-pulse'
 
     alias v="nvim"
-    # anthropic vars
-    set -gx ANTHROPIC_BASE_URL "https://agentrouter.org/"
-    set -gx ANTHROPIC_AUTH_TOKEN sk-gQd0Ms61FrP8HzUs9AJbH19uneYwob9vkqEJx8u86oCtoLbE
-    set -gx ANTHROPIC_MODEL claude-opus-4-6
 
-    # http proxy for terminal
-    set -gx http_proxy http://127.0.0.1:10808
-    set -gx https_proxy http://127.0.0.1:10808
+    # HTTP proxy for local machine only
+    if not set -q SSH_TTY
+        set -gx http_proxy http://127.0.0.1:10808
+        set -gx https_proxy http://127.0.0.1:10808
+    end
 
 end
 set -gx PATH $HOME/.local/bin $PATH
